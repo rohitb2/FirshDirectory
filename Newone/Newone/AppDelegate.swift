6ce25ef5e97b2d2914472316197e9ca2bcb124f7
chetu <chetuimac0149@CHETUIMAC0149.local>
2021-12-24 20:27:39 +0530

    Rohit1

--- Newone/Newone/AppDelegate.swift
+++ Newone/Newone/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    // this is new update
+    // this is new update11
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
